2b90558792f60520e17de632c739d66d298657fd
szymongoralczuk <szymongoralczuk@szymons-macbook-pro.home>
2020-04-09 09:22:25 +0200

    added three

--- demo/src/demo/Main.java
+++ demo/src/demo/Main.java
@@ -3,7 +3,7 @@ package demo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("this is version 2");
+		System.out.println("this is version 3");
 		System.out.println("new feature");
 	}
 
